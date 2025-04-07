9526987a8079ddca6b2d652d12475d60b6fed55b
ArchelProjects <147117568+ArchelProjects@users.noreply.github.com>
2025-04-07 09:24:59 +0800

    Update login.py

--- login.py
+++ login.py
@@ -27,6 +27,6 @@ expected_title = "Dashboard"
 
 if expected_title not in actual_title:
     raise AssertionError("Login Test Failed")
-print("IAMS Login Successfully!!!")
+print("IAMS Login Successfully!")
 
 #driver.close()
