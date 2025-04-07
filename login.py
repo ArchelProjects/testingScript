34e60db1b78e0058f66279a2644c9035f941cef1
ArchelProjects <chelmontiel03@gmail.com>
2025-04-07 09:23:17 +0800

    first commit

---
+++ login.py
@@ -0,0 +1,32 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+service = Service(executable_path="C://browserdriver//geckodriver.exe")
+driver = webdriver.Firefox(service=service)
+driver.get("http://10.10.99.18:8002/login")
+
+#username = driver.find_element(By.NAME, "username")
+username = wait(driver,10).until(EC.presence_of_element_located((By.NAME, "username")))
+password = driver.find_element(By.NAME, "password")
+login_button = driver.find_element(By.ID, "login")
+
+username.send_keys("chlmntl123@gmail.com")
+password.send_keys("Dost2123")
+login_button.click()
+time.sleep(5)
+
+#audit_tab = wait(driver, 10).until(EC.element_to_be_clickable((By.ID, "audit")))
+#audit_tab.click()
+
+actual_title = driver.title
+expected_title = "Dashboard"
+
+if expected_title not in actual_title:
+    raise AssertionError("Login Test Failed")
+print("Login Successfully!!!")
+
+#driver.close()
